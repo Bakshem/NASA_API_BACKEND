85cf8d35e3b57fed2f396d6b74d61766d6624f72
elifb <elifbaksi@gmail.com>
2024-12-19 13:22:59 -0500

    Fix the Base URL

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api' , (req,res) => {
+app.get('/' , (req,res) => {
     res.send('Welcome to the NASA API!')
 });
 
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then ( () => console.log("MongoDB connected"))
     .catch((err) => console.error(err));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
